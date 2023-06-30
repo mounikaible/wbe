ff82b13940fd9b79581e1efaf72d38ca827dca24
Boxonsaas <boxonsaas@gmail.com>
2023-06-30 16:42:49 +0530

    popup

--- components/com_userprofile/views/user/tmpl/default.php
+++ components/com_userprofile/views/user/tmpl/default.php
@@ -284,17 +284,15 @@ $joomla(function() {
 <div class="modal fade" id="notificationModal" tabindex="-1" role="dialog" aria-labelledby="notificationModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="notificationModalLabel">Notifications</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+      <div class="modal-header">   
+        <input type="button" data-dismiss="modal" value="x" class="btn-close1 updat_txt">
+        <h4 class="modal-title"><strong>Notifications</strong></h4>
       </div>
       <div class="modal-body">
         Notification Content
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
